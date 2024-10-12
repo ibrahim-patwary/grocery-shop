d65cb16d75b7f89c062f9398fcbdc48fa464e6e3
ibrahim-patwary <ibrahimpatwary.cse@gmail.com>
2024-10-12 21:57:37 +0600

    final version

--- glossary_store/settings.py
+++ glossary_store/settings.py
@@ -90,16 +90,12 @@ WSGI_APPLICATION = 'glossary_store.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'grocery_shop',
-        'HOST':'localhost',
-        'PORT':'3306',
-        'USER':'root',
-        'password':'',
-    
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
